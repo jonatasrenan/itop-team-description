e12970f1d266eca9b69535cc6d682502a213293e
Jonatas Renan <jonatas@dcc.ufmg.br>
2018-05-02 09:42:07 -0300

    Commit files

---
+++ en.dict.team-description.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	'Class:Team/Attribute:description' => 'Description',
+));
+
+Dict::Add('PT BR', 'Brazillian', 'Brazillian', array(
+	'Class:Team/Attribute:description' => 'Descrição',
+));
+?>
